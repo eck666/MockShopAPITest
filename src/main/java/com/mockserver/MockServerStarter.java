218713a4714b0d11c74ce12c397debab670edea0
eck66 <kilicc.emircan@gmail.com>
2024-05-30 23:28:06 +0300

    mock server added

--- src/main/java/com/mockserver/MockServerStarter.java
+++ src/main/java/com/mockserver/MockServerStarter.java
@@ -1,4 +1,4 @@
-package com.ecommerce.mockserver;
+package com.mockserver;
 
 import org.mockserver.integration.ClientAndServer;
 import org.mockserver.model.HttpRequest;
@@ -40,10 +40,16 @@ public class MockServerStarter {
 
             HttpResponse httpResponse = HttpResponse.response()
                     .withStatusCode(expectation.getHttpResponse().getStatusCode())
-                    .withBody(expectation.getHttpResponse().getBody())
                     .withHeaders(
                             new Header("Content-Type", expectation.getHttpResponse().getHeaders().getContentType())
                     );
+
+            // Check if the body is a string or a JSON object
+            if (expectation.getHttpResponse().getBody() instanceof String) {
+                httpResponse.withBody((String) expectation.getHttpResponse().getBody());
+            } else {
+                httpResponse.withBody(gson.toJson(expectation.getHttpResponse().getBody()));
+            }
 
             mockServer.when(httpRequest).respond(httpResponse);
         }
@@ -79,14 +85,14 @@ public class MockServerStarter {
 
     static class HttpResponseDTO {
         private int statusCode;
-        private String body;
+        private Object body; // Changed type to Object
         private Headers headers;
 
         public int getStatusCode() {
             return statusCode;
         }
 
-        public String getBody() {
+        public Object getBody() { // Changed return type to Object
             return body;
         }
 
